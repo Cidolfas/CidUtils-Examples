8f986db6f39fde96ef94fc8e119cb1cb63d0cb9c
Alex Swaim <apswaim@gmail.com>
2012-06-08 17:01:34 -0500

    Initial commit

---
+++ FSMChase/Scripts/FSMChaseWorldManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class FSMChaseWorldManager : MonoBehaviour {
+	
+	public static FSMChaseWorldManager current;
+	
+	// Use this for initialization
+	void Start () {
+	current = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

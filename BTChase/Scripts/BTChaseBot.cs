52c0638d405f61f6ad91c52528461bd611e4e9cf
Alex Swaim <apswaim@gmail.com>
2012-06-11 13:21:08 -0500

    Finished BT Chase example.

--- BTChase/Scripts/BTChaseBot.cs
+++ BTChase/Scripts/BTChaseBot.cs
@@ -15,6 +15,8 @@ public class BTChaseBot : MonoBehaviour {
 	// Use this for initialization
 	void Awake ()
 	{
+		blackboard.Put("ChasingPlayer", false);
+		
 		// Begin the tree
 		BTNode root = new BTPrioritySelector();
 		bt = new BehaviorTree(root);
@@ -25,6 +27,11 @@ public class BTChaseBot : MonoBehaviour {
 				hulkModeSeq.AddChild(new BTCondition(delegate(){
 						if (target == null) return false;
 						else return target.GetComponent<BTChasePlayer>().state == BTChasePlayer.PlayerState.Hulk;
+					}));
+				// Set "ChasingPlayer" to false in the blackboard
+				hulkModeSeq.AddChild(new BTAction(delegate(){
+						blackboard.Put("ChasingPlayer", false);
+						return BTStatusCode.Success;
 					}));
 				// Are we safe?
 				BTNode botNotSafeSwitch = hulkModeSeq.AddChild(new BTSwitch());
@@ -40,13 +47,43 @@ public class BTChaseBot : MonoBehaviour {
 					botNotSafeSwitch.AddChild(new BTDecAlwaysSucceed());
 			
 			// Player in Normal mode branch
-			
+			BTNode normalModeSeq = root.AddChild(new BTSequence());
+				// Have we noticed the player?
+				normalModeSeq.AddChild(new BTCondition(delegate(){
+						if (target == null) return false;
+						else if (blackboard.Look("ChasingPlayer") != null) {
+							return (bool)blackboard.Look("ChasingPlayer");
+						} else return false;
+					}));
+				// Do we need to be moving closer still?
+				BTNode botTooFarSwitch = normalModeSeq.AddChild(new BTSwitch());
+					// Are we still outside the minimum distance?
+					botTooFarSwitch.AddChild(new BTCondition(delegate(){
+							Vector3 dist = target.transform.position - transform.position;
+							if (dist.sqrMagnitude <= chaseRadius * chaseRadius) return false;
+							else return true;
+						}));
+					// Yes, move closer
+					botTooFarSwitch.AddChild(new BTAction(MoveTowards));
+					// No, let's idle
+					botTooFarSwitch.AddChild(new BTDecAlwaysSucceed());
+		
+			// Player in Idle mode branch
+			BTNode idleModeSeq = root.AddChild(new BTSequence());
+				// If the player is in range, start chasing
+				idleModeSeq.AddChild(new BTAction(delegate(){
+						Vector3 dist = target.transform.position - transform.position;
+						if (dist.sqrMagnitude <= runRadius * runRadius) blackboard.Put("ChasingPlayer", true);
+						return BTStatusCode.Success;
+					}));
+				// Idle
+				idleModeSeq.AddChild(new BTDecAlwaysSucceed());
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		bt.Tick();
 	}
 	
 	// BT functions

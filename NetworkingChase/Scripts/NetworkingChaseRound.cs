4f764e38e8c8715f3b1bfe525433d1b04b95fc8d
Alex Swaim <apswaim@gmail.com>
2012-06-11 20:07:20 -0500

    Changes

--- NetworkingChase/Scripts/NetworkingChaseRound.cs
+++ NetworkingChase/Scripts/NetworkingChaseRound.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class NetworkingChaseRound : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		
 	}
 }
